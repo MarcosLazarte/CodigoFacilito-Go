258b70dd0fdae8a4acb39e7973fb84f946771397
Marcos <marcos.e.lazarte@gmail.com>
2021-11-26 16:58:16 -0300

    Agregado arreglosp2

---
+++ estructura de datos/arreglosP2.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	/*
+		var numeros [5]int //Declaro array de numeros enteros
+		numeros[0] = 100
+		numeros[1] = 200
+		numeros[2] = 300
+		numeros[3] = 400
+		numeros[4] = 500 //Sí asigno "500" dara error
+
+		fmt.Println(numeros[0])
+		fmt.Printf("%d\n", numeros[1])
+		fmt.Print(numeros[2])
+	*/
+}
